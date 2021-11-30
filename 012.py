6d816a96c2d17cb5eb4081bc4ec6cd8df6282a04
Philip Massouh <massouh.3@osu.edu>
2021-11-29 17:25:40 -0700

    more node solutions

--- 012.py
+++ 012.py
@@ -2,7 +2,7 @@ import math
 from functools import reduce
 
 def factors(n):
-	return len(set(reduce(list.__add__,([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
+	return 2 == len(set(reduce(list.__add__,([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
 
 A = [1]
 i = 2
