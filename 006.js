dc007452ec3c0998cd24142a00e0d280f6ebe381
Philip Massouh <philipmassouh@gmail.com>
2023-01-21 16:21:43 -0800

    gitignore

--- 006.js
+++
@@ -1,8 +0,0 @@
-let ssq = 0;
-let sqs = 0;
-for (let i = 1; i < 101; i++) {
-    ssq += i * i;
-    sqs += i
-}
-
-console.log(sqs * sqs - ssq);
--- 016.js
+++
@@ -1,6 +0,0 @@
-sum = 0;
-for (v of BigInt(Math.pow(2,1000)).toString()) {
-    sum += parseInt(v)
-}
-
-console.log(sum)
